f5e768c915c2c00e2e3c6ebc944d767500525812
Prakash Bahadur Chand <prakashbahadurchand@gmail.com>
2024-11-21 06:34:33 +0545

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Prakash Bahadur Chand on 21/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
